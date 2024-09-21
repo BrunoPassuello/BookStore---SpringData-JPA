84831b8fd005a6da1a73e924d94b2d77323a7dca
Bruno Passuello <bruno.fogazzi.passuello@gmail.com>
2024-09-21 16:53:53 -0300

    repositories

---
+++ src/main/java/com/bookstore/jpa/Repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.bookstore.jpa.Repositories;
+
+import com.bookstore.jpa.models.AuthorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AuthorRepository extends JpaRepository<AuthorModel, UUID> {
+}
